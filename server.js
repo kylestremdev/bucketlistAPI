f334409fcc3b56c61e98260fdfbb7f08a1e3bb6f
Kyle Strem <kylestremdev@gmail.com>
2017-04-18 12:02:11 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var express   = require("express"),
+    mongoose  = require("mongoose"),
+    bP        = require("body-parser"),
+    app       = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/bucketlistAPI");
+
+var TaskSchema = mongoose.Schema({
+  objective: {
+    type: String,
+    required: [true, "Must have a task"],
+    validate: {
+      validator: function(v) {
+        return v.length > 3;
+      },
+      message: 'Task must be greater than 3 characters'
+    }
+  }
+}, {timestamps: true});
+
+var Task = mongoose.model("Task", TaskSchema);
+
+app.use(bP.urlencoded({extended:true}));
+app.use(bP.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.get("/tasks", function (req, res) {
+  Task.find({}).exec()
+  .then(function (tasks) {
+    return res.json(tasks);
+  }, function (err) {
+    return res.status(400).send(err);
+  });
+});
+
+app.post("/tasks", function (req, res) {
+  var newTask = new Task({
+    objective: res.body.objective
+  });
+
+  newTask.save().then(function (newtask) {
+    return res.redirect('/tasks');
+  }, function (err) {
+    return res.status(400).send(err);
+  });
+});
+
+app.listen(8000, function () {
+  console.log("Server running on port 8000");
+});
